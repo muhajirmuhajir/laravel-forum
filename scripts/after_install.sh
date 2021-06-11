bf665f21889dd920d121b9a032c4c1e6f0550235
muhajirmuhajir <hajir.nf@gmail.com>
2021-06-11 11:22:04 +0800

    fix code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -10,12 +10,6 @@ ln -s /var/www/public /var/www/html
 chown -R www-data /var/www/html
 
 #install composer
-curl -sS https://getcomposer.org/installer | php
-
-mv composer.phar /usr/local/bin/composer
-
-chmod +x /usr/local/bin/composer
-
 composer install
 
 # This needs to be created at buildtime via codebuild! And key:generate will no longer be necessary
--- scripts/cleanup_apache_conf.sh
+++ scripts/cleanup_apache_conf.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 # rm /etc/apache2/sites-available/*
+
+apt install php7.4-mysql php7.4-cli php7.4-json php7.4-common php7.4-opcache libapache2-mod-php7.4 php7.4-xml php7.4-intl php7.4-gd php7.4-mbstring php7.4-curl php-bcmath -y
+
+cd /home/ubuntu
+
+#install composer
+curl -sS https://getcomposer.org/installer | php
+
+mv composer.phar /usr/local/bin/composer
+
+chmod +x /usr/local/bin/composer
